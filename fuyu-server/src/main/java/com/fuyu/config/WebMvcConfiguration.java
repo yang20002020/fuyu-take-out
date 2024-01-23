39b814bfea145a42e2c2418451d4dbf8097ab1dc
nil2000 <2628636987@qq.com>
2024-01-23 09:06:11 +0800

    店铺营业状态设置---代码开发

--- fuyu-server/src/main/java/com/fuyu/config/WebMvcConfiguration.java
+++ fuyu-server/src/main/java/com/fuyu/config/WebMvcConfiguration.java
@@ -46,7 +46,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
      * @return
      */
     @Bean
-    public Docket docket() {
+    public Docket docket1() {
         log.info("准备生成接口文档....");
         ApiInfo apiInfo = new ApiInfoBuilder()
                 .title("复域美食项目接口文档")
@@ -54,9 +54,31 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
                 .description("复域美食项目接口文档")
                 .build();
         Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .groupName("管理端接口")
                 .apiInfo(apiInfo)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.fuyu.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.fuyu.controller.admin"))
+                .paths(PathSelectors.any())
+                .build();
+        return docket;
+    }
+    /**
+     * 通过knife4j生成接口文档
+     * @return
+     */
+    @Bean
+    public Docket docket2() {
+        log.info("准备生成接口文档....");
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title("复域美食项目接口文档")
+                .version("2.0")
+                .description("复域美食项目接口文档")
+                .build();
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .groupName("用户端接口")
+                .apiInfo(apiInfo)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.fuyu.controller.user"))
                 .paths(PathSelectors.any())
                 .build();
         return docket;
